4b5c8f613f047168b949879e9ca71099c24b8e9c
DmMentor <dm.minaev03@gmail.com>
2021-12-03 11:28:26 +0200

    delete SetUp; add method CreateDefaultCalculator;

--- StringCalculator.Tests/StringCalculator.test.cs
+++ StringCalculator.Tests/StringCalculator.test.cs
@@ -6,17 +6,16 @@ namespace StringCalculator.test
     [TestFixture]
     public class CalculatorTests
     {
-        private Calculator calculator;
-
-        [SetUp]
-        public void SetUp()
+        private Calculator CreateDefaultCalculator()
         {
-            calculator = new Calculator();
+            return new Calculator();
         }
 
         [Test]
         public void Add_EmptyString_ReturnsZero()
         {
+            var calculator = CreateDefaultCalculator();
+
             int result = calculator.Add("");
             Assert.AreEqual(0, result);
         }
@@ -24,6 +23,8 @@ namespace StringCalculator.test
         [Test]
         public void Add_SingleNumber_ReturnsSameNumber()
         {
+            var calculator = CreateDefaultCalculator();
+
             int result = calculator.Add("1");
             Assert.AreEqual(1, result);
         }
@@ -31,6 +32,8 @@ namespace StringCalculator.test
         [Test]
         public void Add_SeveralNumber_ReturnsSumNumbers()
         {
+            var calculator = CreateDefaultCalculator();
+
             int result = calculator.Add("1, 2, 5, 6, 7, 8");
             Assert.AreEqual(29, result);
         }
@@ -38,6 +41,8 @@ namespace StringCalculator.test
         [Test]
         public void Add_SeveralDelimiters_ResurnsSumNumbers()
         {
+            var calculator = CreateDefaultCalculator();
+
             int result = calculator.Add("1\n2, 1\n 6, 0");
             Assert.AreEqual(10, result);
         }
@@ -45,6 +50,8 @@ namespace StringCalculator.test
         [Test]
         public void Add_DefaultDelimiters_ReturnsSumNumbers()
         {
+            var calculator = CreateDefaultCalculator();
+
             int result = calculator.Add("//;\n2;5;6;0");
             Assert.AreEqual(13, result);
         }
@@ -52,7 +59,10 @@ namespace StringCalculator.test
         [Test]
         public void Add_NegativeNumbers_ReturnsException()
         {
-            Exception ex = Assert.Throws<Exception>(() => calculator.Add("-1\n-2, 1\n -6, 0"));
+            var calculator = CreateDefaultCalculator();
+
+            TestDelegate actual = () => calculator.Add("-1\n-2, 1\n -6, 0");
+            Exception ex = Assert.Throws<Exception>(actual);
 
             Assert.AreEqual("negatives not allowed:-1-2-6", ex.Message);
         }
@@ -60,6 +70,8 @@ namespace StringCalculator.test
         [Test]
         public void Add_NumbersBigger_ReturnsSumNumbers()
         {
+            var calculator = CreateDefaultCalculator();
+
             int result = calculator.Add("1, 1002, 1");
             Assert.AreEqual(2, result);
         }
@@ -67,6 +79,8 @@ namespace StringCalculator.test
         [Test]
         public void Add_BigDelimiters_ReturnsSumNumbers()
         {
+            var calculator = CreateDefaultCalculator();
+
             int result = calculator.Add("//[***]\n1***2***3");
             Assert.AreEqual(6, result);
         }
@@ -74,6 +88,8 @@ namespace StringCalculator.test
         [Test]
         public void Add_SeveralDelimiters_ReturnsSumNumbers()
         {
+            var calculator = CreateDefaultCalculator();
+
             int result = calculator.Add("//[%][*]\n1*2%3");
             Assert.AreEqual(6, result);
         }
@@ -81,6 +97,8 @@ namespace StringCalculator.test
         [Test]
         public void Add_BigSeveralDelimiters_ReturnsSumNumbers()
         {
+            var calculator = CreateDefaultCalculator();
+
             int result = calculator.Add("//[***][%%]\n1%%2***3%%6");
             Assert.AreEqual(12, result);
         }
